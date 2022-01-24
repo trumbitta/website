5175735b9625a1f9b8ec1fdf6a94b4fdb9f17ff3
Julian <julian@gitpod.io>
2022-01-24 09:23:24 +0000

    feat: add header

--- src/lib/components/ui-library/feature-table/feature-table.svelte
+++ src/lib/components/ui-library/feature-table/feature-table.svelte
@@ -2,6 +2,7 @@
   import FeatureTableToc from "./desktop/feature-table-toc.svelte";
   import FeatureTableColumn from "./desktop/feature-table-column.svelte";
   import type { FeatureTable } from "./feature-table.types";
+  import FeatureTableColumnMobile from "./mobile/feature-table-column-mobile.svelte";
   export let tableData: FeatureTable;
 
   const getDesktopColumns = () => {
@@ -9,9 +10,14 @@
   };
 </script>
 
-<div class="grid {getDesktopColumns()} gap-4 grid-cols-1">
+<div class="{getDesktopColumns()} gap-4 grid-cols-1 mb-8 hidden md:grid">
   <FeatureTableToc tocData={tableData.toc} />
   {#each tableData.columns as col}
     <FeatureTableColumn featureData={col} />
   {/each}
+</div>
+<div class="grid grid-cols-1 md:hidden gap-4 justify-center">
+  <div class="w-full">
+    <FeatureTableColumnMobile />
+  </div>
 </div>
--- src/lib/components/ui-library/feature-table/mobile/feature-table-column-mobile.svelte
+++ src/lib/components/ui-library/feature-table/mobile/feature-table-column-mobile.svelte
@@ -1,1 +1,34 @@
-<div />
+<script lang="ts">
+  let isShown: boolean = false;
+
+  const toggleIsShown = () => {
+    isShown = !isShown;
+  };
+</script>
+
+<div class="p-4 bg-off-white shadow-xl rounded-3xl max-w-[400px] mx-auto">
+  <div class="p-4 mb-x-small flex flex-col justify-center items-center">
+    <div class="text-center mb-x-small">
+      <h3>Free Plan</h3>
+      <p>Thats a super-neat subtitle</p>
+    </div>
+    <button
+      class="pt-x-small border-t-2 border-divider w-56"
+      on:click={() => toggleIsShown()}
+    >
+      <div class="underline">
+        {#if isShown}
+          Hide Features
+        {:else}
+          View all Features
+        {/if}
+      </div>
+      <img
+        src="/arrow.svg"
+        alt="Arrow Toggle"
+        class="h-6 w-6 mt-macro mx-auto duration-200"
+        class:rotate-180={isShown}
+      />
+    </button>
+  </div>
+</div>
